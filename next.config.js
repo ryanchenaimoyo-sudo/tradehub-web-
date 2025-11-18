1514c4772d33dc97b6180b5914c4e03348f41799
ryanchenaimoyo-sudo <ryanchenaimoyo@gmail.com>
2025-11-18 21:09:14 +0200

    Add Next.js configuration with strict mode and image domains

---
+++ next.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: { domains: ['images.unsplash.com', 'lh3.googleusercontent.com'] }
+}
+module.exports = nextConfig;
